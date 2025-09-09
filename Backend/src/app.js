44e8c0958c3df3811c2d6b3825af974b883fe74b
banshita rout <banshita.rout.1@gmail.com>
2025-09-10 03:48:30 +0530

    updating meet

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,49 +1,39 @@
 import express from "express";
-import{createServer} from "node:http"; // used for socketio for raw http
+import { createServer } from "node:http";
+
 import { Server } from "socket.io";
-import connectTOsocket from "./controllers/socketmaneger.js";
- import mongoose from "mongoose";
- import cors from "cors";
- import userRoutes from "./routes/userrouter.js";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
-const server = createServer(app); 
- connectTOsocket(server);
+const server = createServer(app);
+const io = connectToSocket(server);
 
-app.set("port", process.env.PORT || 8000);
+
+app.set("port", (process.env.PORT || 8000))
 app.use(cors());
-app.use(express.json({ limit:"40Kb"}));
-app.use(express.urlencoded({limit:"40kb",extended:true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-
-app.use("/api/v1/users",userRoutes);
-
-
-
-
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://banshitarout1:banshitawebdev@cluster0.1h2x1me.mongodb.net/");
-    console.log(" DB CONNECTED");
-  } catch (err) {
-    console.error(" DB Connection Failed:", err);
-  }
-};
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://banshitarout1:banshitawebdev@cluster0.1h2x1me.mongodb.net/")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
 
 
 
+}
 
 
-
-
-server.listen(8000,()=>{
-    console.log("listening on port 8000" )
-});
 
 start();
-
-
-
-
-
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import LandingPage from "./pages/landing";
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
+import History from './pages/history';
 
 function App() {
   return (
-    <>
+    <div className="App">
+
       <Router>
-        <Routes>
-          {/* <Route path='./home' element=/> */}
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
+
+        <AuthProvider>
+
+
+          <Routes>
+
+            <Route path='/' element={<LandingPage />} />
+
+            <Route path='/auth' element={<Authentication />} />
+
+            <Route path='/home's element={<HomeComponent />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/:url' element={<VideoMeetComponent />} />
+          </Routes>
+        </AuthProvider>
+
       </Router>
-    </>
+    </div>
   );
 }
 
